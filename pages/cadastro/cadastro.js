4bfd7a00ca41e65a507fa58597807922c5553712
Pedro Goncalves de Almeida <2301675@SALAS.AULAS>
2023-10-02 20:12:07 -0300

    fixing keyconfirm

--- pages/cadastro/cadastro.js
+++ pages/cadastro/cadastro.js
@@ -1,18 +1,24 @@
 $(document).ready(function () {
     $("#btn").click(function () {
-      console.log("Botão clicado");
-      let email = $("#email").val();
-      let name = $("#name").val();
-      let key = $("#key").val();
-      let keyconfirm = $("#keyconfirm").val();
-      if (email == "" || key == "" || name == "" || keyconfirm == "") {
-        bootbox.alert({
-          message:
-            "Revise os campos preenchidos. Ambos os campos são obrigatórios!",
-          backdrop: true,
-        });
-      }
+        console.log("Botão clicado");
+        let email = $("#email").val();
+        let name = $("#name").val();
+        let key = $("#key").val();
+        let keyconfirm = $("#keyconfirm").val();
+        if (key != keyconfirm) {
+            bootbox.alert({
+                message:
+                    "Revise os campos preenchidos. Os campos de senha e confirmar a senha precisam ser iguais!",
+                backdrop: true,
+            });
+        }
+        if (email == "" || key == "" || name == "" || keyconfirm == "") {
+            bootbox.alert({
+                message:
+                    "Revise os campos preenchidos. Ambos os campos são obrigatórios!",
+                backdrop: true,
+            });
+        }
     });
-  });
-  
-  
+});
+
